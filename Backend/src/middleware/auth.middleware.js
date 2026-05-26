0001011f6dbebc1a8c60def997f3ed5d5ce3ffce
Yug2op <mr.yugank.2000@gmail.com>
2026-05-27 02:08:48 +0530

    feat: remove debug logging for token in protectRoute middleware

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -4,7 +4,6 @@ import { User } from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-    console.log(token);
 
     if (!token) {
       return res.status(410).json({ message: "Unauthorized - No token provided" });
